c264f3efeae6eb53c55ebeab458e7754acdc3b2e
stephenwiafe <stephwiafe18@gmail.com>
2023-02-15 11:41:41 -0500

    Add new changes

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,12 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+
+const yourName : String = "Stephen Wiafe";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "Computer Science";
+let classification : String = "Sophomore";
+let oneExcite : String = "The finals";
+let anyConcerns : String = "None";
+let funFact : String = "I like web development";
+=
