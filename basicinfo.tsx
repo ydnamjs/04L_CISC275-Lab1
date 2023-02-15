1526d6bda47fc5f29e900bac4b2fa9df16cce108
ryansexton <rjsexton1210@gmail.com>
2023-02-15 10:44:11 -0500

    commit

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,10 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+const yourName : String = "Ryan Sexton";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "Computer Science";
+let classification : String = "sophomore";
+let oneExcite : String = "Learning how to work with software";
+let anyConcerns : String = "remembering git commands";
+let funFact : String = "My favorite baseball team is the New York Mets";
