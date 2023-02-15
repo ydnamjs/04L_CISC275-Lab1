a3bff402f1787065717d887e18ee4329454fa325
patelsap <patelsap@udel.edu>
2023-02-15 10:43:33 -0500

    edited basicinfo by adding personal info.

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,10 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+const yourName : String = "Shay Patel";
+let yourPronouns : String = "he/him";
+const yourMajor : String = "Computer Science";
+let classification : String = "soph";
+let oneExcite : String = "Learning about software engineering";
+let anyConcerns : String = "no concerns at the moment";
+let funFact : String = "I'm a fan of the Chicago Bulls";
